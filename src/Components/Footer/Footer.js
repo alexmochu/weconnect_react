e2ea33f4c11d0d8418f4662825a64567f9815a31
alexmochu <mochualex4@gmail.com>
2018-06-12 12:54:53 +0300

    add header, footer and homapage components

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+
+import './Footer.css';
+
+class Footer extends Component {
+  
+  render() {
+      return (
+          <Menu fixed='bottom'>
+                <p>2018 weConnect UI Templates Project by <a href="https://github.com/alexmochu">alexmochu</a>.</p>
+          </Menu>
+      );
+  }
+}
+
+export default Footer;
