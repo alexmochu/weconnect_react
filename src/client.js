a2629efecbb8cf6176a1fe92bf591cc58ecf8c0d
alexmochu <mochualex4@gmail.com>
2018-12-17 15:46:12 +0300

    Update app

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
+// localhost --> http://127.0.0.1:5000/
+// heroku --> https://weconnect-v2-heroku.herokuapp.com/
 var axiosInstance = axios.create({
-  baseURL: "https://weconnect-v2-heroku.herokuapp.com/"
+  baseURL: "http://127.0.0.1:5000/"
 });
 export default axiosInstance;
